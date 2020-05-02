0c90c0cb75098ceaf77a71bf93ff58f5d2ae3d47
kgejara <47546861+kgejara@users.noreply.github.com>
2020-05-01 21:39:17 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Require TF version to most recent
 terraform {
-  required_version = "=0.12.10"
+  required_version = "=0.12.24"
 }
 
 # Download any stable version in AWS provider of 2.19.0 or higher in 2.19 train
@@ -12,10 +12,21 @@ provider "aws" {
 # Call the seed_module to build our ADO seed info
 module "ado_seed" {
   source                       = "./modules/ado_seed"
-  name_of_s3_bucket            = "s3-bucket-name-kyler-ue1-tfstate"
+  name_of_s3_bucket            = "s3-bucket-name-kumar-ue1-tfstate"
   dynamo_db_table_name         = "aws-locks"
   iam_user_name                = "AzureDevOpsIamUser"
   ado_iam_role_name            = "AzureDevOpsIamRole"
   aws_iam_policy_permits_name  = "AzureDevOpsIamPolicyPermits"
   aws_iam_policy_assume_name   = "AzureDevOpsIamPolicyAssume"
+}
+
+terraform {
+  required_version = "=0.12.24"
+  backend "s3" {
+    bucket         = "s3-bucket-name-kumar-ue1-tfstate"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "aws-locks"
+    encrypt        = true
+  }
 }
